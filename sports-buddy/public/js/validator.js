5027de2831997e6dc3cc9984313031eaba696378
mohit00m <mohitmajoka290@gmail.com>
2026-02-17 12:18:56 +0530

    Delete sports-buddy/public directory

--- sports-buddy/public/js/validator.js
+++
@@ -1,7 +0,0 @@
-export function validateText(value, fieldName) {
-  if (!value || value.trim().length < 2) {
-    alert(fieldName + " must be at least 2 characters.");
-    return false;
-  }
-  return true;
-}
